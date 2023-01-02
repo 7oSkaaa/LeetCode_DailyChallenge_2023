1f0b1be2aec4d36038bdfb9cccbae148dd5f11d2
Ahmed Hossam <63050133+7oSkaaa@users.noreply.github.com>
2023-01-02 13:34:42 +0200

    Create 2- Detect Capital.cpp

---
+++ January/2- Detect Capital.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        // count capital letters
+        int CapitalLetters =  count_if(word.begin(), word.end(), [&](char c){ 
+            return isupper(c); 
+        });
+        
+        // check the three conditions if there is anyone valid
+        bool isCapital = false;
+        isCapital |= (CapitalLetters == word.size());
+        isCapital |= (CapitalLetters == 0);
+        isCapital |= CapitalLetters == 1 && isupper(word.front());
+        
+        // return the answer
+        return isCapital;
+    }
+};
