4fe015d650edf2701f488649592886e3ac62d9b8
Mahmoud Aboelsoud <56567087+aboelsooud@users.noreply.github.com>
2023-01-02 15:24:46 +0200

    Create 2- Detect Capital (Mahmoud Aboelsoud).cpp

---
+++ January/2- Detect Capital/2- Detect Capital (Mahmoud Aboelsoud).cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int cnt_cap = 0;
+        
+        // count capital characters in the word
+        for(auto&i: word){
+            if(isupper(i)) cnt_cap++;
+        }
+
+        // check the cases that define the right usage of capitals:
+        //   1- all letters are capitals
+        //   2- all letters are small
+        //   3- only the first letter is capital    
+        if(cnt_cap == 0 || cnt_cap == word.size()) return true;
+        else if(cnt_cap == 1 && isupper(word[0])) return true;
+
+
+        // return false otherwise
+        return false;
+    }
+};
