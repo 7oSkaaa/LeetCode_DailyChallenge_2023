0ccc3c30ae8bc5d7c2f766ea5406b2ce88f347ed
Eman <emoelsayed2192002@gmail.com>
2023-01-04 10:46:07 +0200

    create 4- Minimum Rounds to Complete All Tasks (Eman Elsayed).cpp

---
+++ January/4- Minimum Rounds to Complete All Tasks/4- minimum Rounds to complete All Tasks (Eman Elsayed).cpp
@@ -0,0 +1,36 @@
+class Solution
+{
+public:
+    int minimumRounds(vector<int> &tasks)
+    {
+        int cnt = 0;
+        map<int, int> mp; // Store the frequencies in the map.
+        for (auto &i : tasks)
+            mp[i]++;
+
+        // Iterate over the task's frequencies
+        for (auto &i : mp)
+        {
+            // If the frequency is 1, it's not possible to complete tasks
+            if (i.second == 1)
+            {
+                return -1;
+            }
+            // Group all the tasks in triplets to get minimum Rounds
+            cnt += i.second / 3;
+            /*
+                 If count % 3 = 1 , groups of triplets and 2 pairs.
+                 Ex : tasks = [5 , 5 , 5 , 5]
+                 we divide it to groups [5 , 5] , [5 , 5] is correct [5, 5, 5][5] is not
+                 If count % 3 = 2 , groups of triplets and 1 pair
+                 Ex : tasks = [5, 5, 5, 5, 5]
+                 we divide it to groups [5, 5, 5][5 , 5]
+                 NOTE: COUNT % 3  , the result always will be 0 , 1 , 2
+            */
+            if (i.second % 3 != 0)
+                cnt++;
+        }
+        // Return the Answer of the Problem
+        return cnt;
+    }
+};
