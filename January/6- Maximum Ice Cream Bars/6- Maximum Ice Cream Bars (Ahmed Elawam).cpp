a008300fd39957e30dc653d4be12f7a03fc7d38d
Ahmed Elawam <54435155+ArEl3wam@users.noreply.github.com>
2023-01-06 13:21:01 +0200

    Update 6- Maximum Ice Cream Bars (Ahmed Elawam).cpp

--- January/6- Maximum Ice Cream Bars/6- Maximum Ice Cream Bars (Ahmed Elawam).cpp
+++ January/6- Maximum Ice Cream Bars/6- Maximum Ice Cream Bars (Ahmed Elawam).cpp
@@ -2,10 +2,13 @@
 class Solution {
 public:
     int maxIceCream(vector<int>& costs, int coins) {
+        // first sort costs of ice creams ascending.
         sort(costs.begin(),costs.end());
-        int cnt =0;
+        int cnt =0; // counter
 
         for (auto e : costs){
+            // iterate through ice creams, if cost of each is affordable just buy it :)
+            // and increase count of bought ice creams by 1, decrease coins by ice cream price 
             if (e<=coins){
                 coins-=e;
                 cnt++;
