88fed4cd8605d733e30502a2c16d0d41132b261f
Abrar-2510 <abrarbero2510@gmail.com>
2023-01-06 21:01:56 +0200

    comments added

--- January/1- Word Pattern/1- Word Pattern (Abrar Mohamed).cpp
+++ January/1- Word Pattern/1- Word Pattern (Abrar Mohamed).cpp
@@ -3,16 +3,20 @@
 class Solution {
 public:
     bool wordPattern(string pattern, string str) {
+ // to assign chars from the pattern to words from string.
         unordered_map<char, string> char_map;
+//vise versa
         unordered_map<string, char> word_map;
-        
-        stringstream s(str);
+  // for insertion and extraction of data to/from the string objects      
+        stringstream s(str); 
+// A string to store the word on each iteration.
         string word;
         int n = pattern.size(), i = 0;
-        
+        // while there is words
         while(s >> word){
             if(i == n) return false;
             char c = pattern[i];
+// check if the number of words equal or not the chars of the pattern string.
             if(char_map.count(c) != word_map.count(word)) return false;
             if(char_map.count(c)){
                 if( (word_map[word] != c) || (char_map[c] != word)) return false;
