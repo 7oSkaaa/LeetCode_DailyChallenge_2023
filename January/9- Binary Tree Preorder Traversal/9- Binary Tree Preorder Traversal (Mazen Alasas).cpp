5071e51f84e9731920a8065a84f830f65b1ce77d
Mazen Ahmed Alasas <112027614+mazen-alasas@users.noreply.github.com>
2023-01-09 11:08:00 +0200

    Update 9- Binary Tree Preorder Traversal (Mazen Alasas).cpp

--- January/9- Binary Tree Preorder Traversal/9- Binary Tree Preorder Traversal (Mazen Alasas).cpp
+++ January/9- Binary Tree Preorder Traversal/9- Binary Tree Preorder Traversal (Mazen Alasas).cpp
@@ -5,11 +5,11 @@ public:
     void preorder(TreeNode*root){
         if (root == NULL)   // If node(root) is empty(null)
             return;
-        nodes.push_back(root -> val);   // Push the value of node
+        nodes.push_back(root -> val);   // Push the value of node First
         // PreOreder Traversal  -->  ( root -> left -> right )  
         // Using Recursion
-        preorder(root -> left);    // Work on the left first
-        preorder(root -> right);    // Ihen on the right
+        preorder(root -> left);    // Thenn the left first
+        preorder(root -> right);    // Then on the right
     }
     vector<int> preorderTraversal(TreeNode* root) {
         preorder(root);    // Call Preorder Function
