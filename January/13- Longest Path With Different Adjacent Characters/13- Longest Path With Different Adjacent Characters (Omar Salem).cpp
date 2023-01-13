2397d567445e7b27db9b945c09d3f6d26aa0a01c
Omar Salem <87977484+omarsalem33@users.noreply.github.com>
2023-01-13 07:34:32 +0200

    Update 13- Longest Path With Different Adjacent Characters (Omar Salem).cpp

--- January/13- Longest Path With Different Adjacent Characters/13- Longest Path With Different Adjacent Characters (Omar Salem).cpp
+++ January/13- Longest Path With Different Adjacent Characters/13- Longest Path With Different Adjacent Characters (Omar Salem).cpp
@@ -3,32 +3,32 @@ class Solution {
 public:
     int DFS (int node , int &globalMax , vector < vector < int > > &tree , 
     string &s ){
-        priority_queue<int, vector<int>, greater<int>> MaxTwo;
+        priority_queue<int, vector<int>, greater<int>> MaxTwo; // create a pq to return Max 2 values 
         MaxTwo.push(0),MaxTwo.push(0); 
 
-        for(auto &child : tree[node])
+        for(auto &child : tree[node]) // DFS on tree
         {
             int MaxFromChild = DFS(child , globalMax , tree , s);
             
-            if(s[child] != s[node]){
-                 MaxTwo.push(MaxFromChild);
+            if(s[child] != s[node]){ // chech two node are valid ---> a -> a not valid / a->b valid 
+                 MaxTwo.push(MaxFromChild); // get a max 2 values 
                  MaxTwo.pop();
             }
         }
         int Max2 = MaxTwo.top() ; MaxTwo.pop();
         int Max1 = MaxTwo.top() ; MaxTwo.pop();
 
-        globalMax = max(globalMax,1+Max1+Max2);
+        globalMax = max(globalMax,1+Max1+Max2); // return max path 
 
         return Max1+1;
     }
     int longestPath(vector<int>& parent, string s) {
         int n = parent.size();
-        vector < vector < int > >tree(n);
+        vector < vector < int > >tree(n); // convert arr to tree 
         for(int i = 1 ; i < n ;i++)
             tree[parent[i]].push_back(i);
         int globalMax = 0;
-        DFS(0,globalMax,tree,s);
+        DFS(0,globalMax,tree,s); // call DFS
         return globalMax; 
     }
 
