d2fcf2bf66bfa17fb82c39092ea950a2dfa54937
7oSkaaa <ahmed.7oskaa@gmail.com>
2023-02-02 14:01:44 +0200

    refactor: change in script for monthly folder

--- .github/scripts/Fetch_Daily_Problem.py
+++ .github/scripts/Fetch_Daily_Problem.py
@@ -7,27 +7,29 @@ import os
 def get_daily_problem():
     # headers to send to the graphql
     headers = {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
     }
 
     # json data to send to the graphql
     json_data = {
-        'query': 'query questionOfToday {\n\tactiveDailyCodingChallengeQuestion {\n\t\tdate\n\t\tuserStatus\n\t\tlink\n\t\tquestion {\n\t\t\tacRate\n\t\t\tdifficulty\n\t\t\tfreqBar\n\t\t\tfrontendQuestionId: questionFrontendId\n\t\t\tisFavor\n\t\t\tpaidOnly: isPaidOnly\n\t\t\tstatus\n\t\t\ttitle\n\t\t\ttitleSlug\n\t\t\thasVideoSolution\n\t\t\thasSolution\n\t\t\ttopicTags {\n\t\t\t\tname\n\t\t\t\tid\n\t\t\t\tslug\n\t\t\t}\n\t\t}\n\t}\n}\n',
-        'operationName': 'questionOfToday',
+        "query": "query questionOfToday {\n\tactiveDailyCodingChallengeQuestion {\n\t\tdate\n\t\tuserStatus\n\t\tlink\n\t\tquestion {\n\t\t\tacRate\n\t\t\tdifficulty\n\t\t\tfreqBar\n\t\t\tfrontendQuestionId: questionFrontendId\n\t\t\tisFavor\n\t\t\tpaidOnly: isPaidOnly\n\t\t\tstatus\n\t\t\ttitle\n\t\t\ttitleSlug\n\t\t\thasVideoSolution\n\t\t\thasSolution\n\t\t\ttopicTags {\n\t\t\t\tname\n\t\t\t\tid\n\t\t\t\tslug\n\t\t\t}\n\t\t}\n\t}\n}\n",
+        "operationName": "questionOfToday",
     }
 
     # get the response from the graphql
-    response = requests.post('https://leetcode.com/graphql', headers=headers, json=json_data)
+    response = requests.post(
+        "https://leetcode.com/graphql", headers=headers, json=json_data
+    )
     response = response.json()
-    response = response['data']['activeDailyCodingChallengeQuestion']
+    response = response["data"]["activeDailyCodingChallengeQuestion"]
 
     # return the problem name
-    return response['question']['title'], response['date']
+    return response["question"]["title"], response["date"]
 
 
 def parse_date(date):
     # split the date
-    date = date.split('-')
+    date = date.split("-")
 
     # get the month
     month = date[1]
@@ -41,14 +43,27 @@ def parse_date(date):
 
 def parse_month(month):
 
-    months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    months = [
+        "01- January",
+        "02- February",
+        "03- March",
+        "04- April",
+        "05- May",
+        "06- June",
+        "07- July",
+        "08- August",
+        "09- September",
+        "10- October",
+        "11- November",
+        "12- December",
+    ]
 
     return months[int(month) - 1]
 
 
 def read_data():
     # read the data from the file
-    with open('.github/data/problems.json', 'r') as file:
+    with open(".github/data/problems.json", "r") as file:
         data = json.loads(file.read())
 
     # return the data
@@ -57,24 +72,24 @@ def read_data():
 
 def write_data(data):
     # write the data to the file
-    with open('.github/data/problems.json', 'w') as file:
+    with open(".github/data/problems.json", "w") as file:
         json.dump(data, file, indent=4)
 
 
 def append_folder(problem_name, day, month):
 
-    print(f'{month}/{day}- {problem_name} folder created successfully!')
+    print(f"{month}/{day}- {problem_name} folder created successfully!")
 
     # append to folder
-    os.makedirs(f'{month}/{day}- {problem_name}', exist_ok=True)
+    os.makedirs(f"{month}/{day}- {problem_name}", exist_ok=True)
 
     # change the path to the folder
-    os.chdir(f'{month}/{day}- {problem_name}')
+    os.chdir(f"{month}/{day}- {problem_name}")
 
     # create .gitkeep file
-    open('.gitkeep', 'a').close()
+    open(".gitkeep", "a").close()
 
-    return f'{month}/{day}- {problem_name}'
+    return f"{month}/{day}- {problem_name}"
 
 
 def main():
@@ -97,10 +112,7 @@ def main():
     monthly_problems = data[month]
 
     # add the problem to the file
-    problem_to_add = {
-        "day": day,
-        "title": problem_name
-    }
+    problem_to_add = {"day": day, "title": problem_name}
 
     if problem_to_add not in monthly_problems:
         monthly_problems.append(problem_to_add)
