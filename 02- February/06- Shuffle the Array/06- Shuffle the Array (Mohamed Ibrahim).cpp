27ac004ef448c1245316fdcf6624e7a9f077141a
Mohamed Ibrahim <mohamedibra3232@gmail.com>
2023-02-06 15:28:33 +0200

    Add 06- Shuffle the Array (Mohamed Ibrahim).cpp

--- 02- February/06- Shuffle the Array/06- Shuffle the Array (Mohamed Ibrahim).cpp
+++ 02- February/06- Shuffle the Array/06- Shuffle the Array (Mohamed Ibrahim).cpp
@@ -2,7 +2,7 @@
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
-        vector<int>ans; // to store the answer
+        vector<int>ans;  // to store the answer
          for(int i = 0; i < n; i++){
              /*for each iteration we will push one from the start and one from the middle*/
             ans.push_back(nums[i]);
