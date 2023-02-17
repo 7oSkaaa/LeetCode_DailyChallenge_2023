2e2734f2264c88bbac4e1610b126a4d9faf265b4
7oSkaaa <ahmed.7oskaa@gmail.com>
2023-02-17 17:15:59 +0200

    refactor: add more comment to my solution in day 17

--- 02- February/17- Minimum Distance Between BST Nodes/17- Minimum Distance Between BST Nodes (Ahmed Hossam).cpp
+++ 02- February/17- Minimum Distance Between BST Nodes/17- Minimum Distance Between BST Nodes (Ahmed Hossam).cpp
@@ -3,26 +3,34 @@
 class Solution {
 public:
 
-    // get values of the tree
+    // get values of the tree in increasing order using Inorder traverse
     vector < int > values;
 
-    // Left -> Root -> Right
+    // Left -> Root -> Right (Inorder traverse)
     void Inorder(TreeNode* root){
         if(!root) return;
+        // go to the left subtree
         Inorder(root -> left);
+        
+        // get the value of the root
         values.push_back(root -> val);
+
+        // go to the right subtree
         Inorder(root -> right);
     }
 
     int minDiffInBST(TreeNode* root) {
-        // lets make Inorder traverse to get the tree sorted in increasing order
+        // get values of the tree in increasing order using Inorder traverse
         Inorder(root);
 
-        // let get the min_diff between any two adjacent numbers
+        // get the minimum difference between two adjacent values
         int min_diff = 1e9;
+
+        // values[i] - values[i - 1] is the difference between two adjacent values
         for(int i = 1; i < values.size(); i++)
             min_diff = min(min_diff, values[i] - values[i - 1]);
 
+        // return the minimum difference
         return min_diff;
     }
 };
