2b801c7a7bcd006fc02fed81a8f20579594fb4ff
ahmedgamal2212 <ahmedgemy2212@gmail.com>
2023-02-17 11:07:34 +0200

    Add '17- Minimum Distance Between BST Nodes (Ahmed Gamal).cpp'

--- 02- February/17- Minimum Distance Between BST Nodes/17- Minimum Distance Between BST Nodes (Ahmed Gamal).cpp
+++ 02- February/17- Minimum Distance Between BST Nodes/17- Minimum Distance Between BST Nodes (Ahmed Gamal).cpp
@@ -1,3 +1,5 @@
+// Author: Ahmed Gamal
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -10,29 +12,44 @@
  * };
  */
 
-# 
+// for each node, find the closest node in the set (itself or the next or the previous)
+// and update the minimum distance
+// then insert the current node in the set
+// and return the minimum distance
+
 
 class Solution {
+    // infinity value to return if the tree is empty
     #define inf 2000'000'000
+
+    // set to store the nodes and find the closest node
     set<int> st;
 public:
     
     int minDiffInBST(TreeNode* root) {
+        // if the current node is null, return infinity
         if(not root)
             return inf;
         
+        // find the closest node (initially infinity)
         int mn = inf;
         auto pos = st.lower_bound(root -> val);
+
+        // if the current node is in the set, the minimum distance is 0
         if(pos != st.end() and *pos == root -> val){
             mn = 0;
         }else{
+            // otherwise, find the closest node
             if(pos != st.end())
                 mn = min(mn, abs(*pos - root -> val));
             if(pos != st.begin())
                 mn = min(mn, abs(*prev(pos) - root -> val));
         }
 
+        // insert the current node in the set
         st.insert(root -> val);
+
+        // return the minimum distance between the current node and its closest node
         return min({mn, minDiffInBST(root -> left), minDiffInBST(root -> right)});
     }
 };
