dad75712af8f9e7ce454c427b900319bbec31a3e
Ahmad Abdelwhab <101134554+Ahmadabdelwhab@users.noreply.github.com>
2023-02-18 13:59:58 +0200

    Update 18- Invert Binary Tree (ahmad abdelwhab).cpp

--- 02- February/18- Invert Binary Tree/18- Invert Binary Tree (ahmad abdelwhab).cpp
+++ 02- February/18- Invert Binary Tree/18- Invert Binary Tree (ahmad abdelwhab).cpp
@@ -1,14 +1,4 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+//Author : Ahamad abdelwhab
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
