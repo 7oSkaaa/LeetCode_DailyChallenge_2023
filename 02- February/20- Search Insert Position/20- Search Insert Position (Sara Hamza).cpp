ca04a39bb1b4bc5b6305528eb25b2f6a4fc20a40
Sara Hamza <sarasaadoun4@gmail.com>
2023-02-20 07:35:26 +0200

    modify comment in 20- Search Insert Position (Sara Hamza).cpp

--- 02- February/20- Search Insert Position/20- Search Insert Position (Sara Hamza).cpp
+++ 02- February/20- Search Insert Position/20- Search Insert Position (Sara Hamza).cpp
@@ -5,7 +5,7 @@ public:
     int searchInsert(vector<int>& nums, int target) {
         /*
             lower_bound returns an iterator pointing to the first element
-            in nums which has a value not less than val. (equal or greater than val)
+            in nums which has a value not less than target. (equal or greater than target)
         */
         return lower_bound(nums.begin(), nums.end(), target) - nums.begin();
 
