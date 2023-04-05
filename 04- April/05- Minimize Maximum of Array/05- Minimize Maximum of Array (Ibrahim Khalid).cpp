6d0d1f54a52c9f1f48778e6e135a0a06a4ea838f
Ibrahim Khaild <103538501+Hima-khalid@users.noreply.github.com>
2023-04-05 17:17:11 +0200

    Update 05- Minimize Maximum of Array (Ibrahim Khalid).cpp

--- 04- April/05- Minimize Maximum of Array/05- Minimize Maximum of Array (Ibrahim Khalid).cpp
+++ 04- April/05- Minimize Maximum of Array/05- Minimize Maximum of Array (Ibrahim Khalid).cpp
@@ -1,65 +1,25 @@
-
-// Author: Mohamed Emara
-
+// Author: Ibrahim Khalid
 class Solution {
 public:
-    vector<long long>v;
-    long long n;
-
-    bool can(long long x)
-    {
-        // to save the original vector after performing some operations.
-        vector<long long>tmp;
-        tmp = v;
-
-        for(int i=n-1; i>0; i--)
-        {
-            // has to perform operation on index i to make the result = x
-            if(v[i] > x)
-            {
-                long long diff = v[i] - x;
-                v[i] = x;
-                v[i-1] += diff;
-            }
-        }
-
-        // if after modifying the vector, there is no element greater than x
-        // so, X is a valid solution
-        int ok = 0;
-        if(*max_element(v.begin(), v.end()) <= x)
-            ok = 1;
-        else ok = 0;
-  
-        // restore the original vector.
-        v = tmp;
-        return ok;
-    }
-
     int minimizeArrayValue(vector<int>& nums) {
-        // Fill the global vector "v" to access it in 'can' function.
-        for(auto it : nums)
-            v.push_back(it);
+        int sz=nums.size();
 
-        n = nums.size();
+        // store first value because the result won't less it
 
-        // The result is some value between 1 and max element in the original vector
-        long long l = 1, r = *max_element(nums.begin(), nums.end());
-        long long ans = r;
-        
-        while(l <= r)
-        {
-            long long mid = l + (r-l) / 2;
-            // at every mid, if it's a valid ans ===> see a better one
-            // otherwise, search in the larger side for a valid ans
-            if(can(mid)){
-                r = mid-1;
-                ans = mid;
-            }
-            else 
-                l = mid+1;
+        int res=nums[0];
+
+        int f;
+
+        double sum=nums[0];
+        //We want to distribute the numbers equal from left to right,                  and compare them with the first and take max by collecting them and dividing them by their number
+        for(int i=1;i<sz;i++){
+
+            sum+=nums[i];
+
+            f=ceil(sum/(i+1));
+            
+            res=max(res,f);
         }
-
-        return ans;
+        return res;
     }
 };
-
