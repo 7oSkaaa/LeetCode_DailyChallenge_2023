128e64c07aa6b8dcc91e7abe303ab5a5f634c62f
Ahmed Hossam <63050133+7oSkaaa@users.noreply.github.com>
2023-07-26 14:47:33 +0300

    Create 26- Minimum Speed to Arrive on Time (Ahmed Hossam).cpp

---
+++ 07- July/26- Minimum Speed to Arrive on Time/26- Minimum Speed to Arrive on Time (Ahmed Hossam).cpp
@@ -0,0 +1,39 @@
+// Author: Ahmed Hossam
+
+class Solution {
+public:
+    int minSpeedOnTime(vector<int>& dist, double hour) {
+        // Get the number of distances in the vector
+        int n = dist.size();
+        
+        // Define a lambda function to check if a given speed is good for finishing in time
+        auto is_good = [&](double speed){
+            double time = 0;
+            // Iterate through all distances except the last one
+            for(int i = 0; i < n - 1; i++)
+                // Calculate the time taken for each distance and add it to the total time
+                time += ceil(dist[i] / speed);
+            
+            // Add the time taken to cover the last distance
+            time += dist.back() / speed;
+            
+            // Check if the total time is less than or equal to the specified hour
+            return time <= hour;
+        };
+        
+        // Initialize the search space for the binary search
+        int l = 1, r = 1e7, ans = -1;
+        
+        // Perform binary search to find the minimum speed
+        while(l <= r){
+            // Calculate the middle speed
+            int m = l + (r - l) / 2;
+            
+            // Update the search space based on whether the current speed is good or not
+            (is_good(m) ? r = m - 1, ans = m : l = m + 1);
+        }
+        
+        // Return the minimum speed found
+        return ans;
+    }
+};
